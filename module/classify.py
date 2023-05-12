dfd813ace3fe0dbfc6a8badcaed61e77f1051212
LIvanoff <drfrily@gmail.com>
2023-05-12 15:18:52 +0300

    Update model classify.py

--- module/classify.py
+++ module/classify.py
@@ -22,7 +22,7 @@ def classify(PATH):
     labels = {0: 'borsch', 1: 'four cheeses', 2: 'khinkali', 3: 'pancake', 4: 'pelmeni', 5: 'pepperoni', 6: 'shawarma',
               7: 'shchi'}
 
-    return labels[predicted]
+    return labels[predicted.detach().numpy()[0]]
 
 
 # print(classify('dataset/val/pelmeni/pelmeni0.jpg'))
